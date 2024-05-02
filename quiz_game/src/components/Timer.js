180f044c0db7ae76cf710fd6f485e2d2ac90aabc
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2024-05-02 14:26:41 +0200

    I created the hooks and the countdonw function

--- quiz_game/src/components/Timer.js
+++ quiz_game/src/components/Timer.js
@@ -4,6 +4,25 @@ export const Timer = ({ onRestart }) => {
   const initialTime = 5 * 60;
   const [timeRemaining, setTimeRemaining] = useState(initialTime);
   let timerId;
+  useEffect(() => {
+    const countdown = () => {
+      timerId = setInterval(() => {
+        setTimeRemaining((prevTime) => {
+          if (prevTime > 0) {
+            return prevTime - 1;
+          } else {
+            clearInterval(timerId);
+            onRestart();
+            return initialTime;
+          }
+        });
+      }, 1000);
+    };
+    
+    countdown();
+
+    return () => clearInterval(timerId);
+  }, [onRestart]);
 
   return (
     <div>
