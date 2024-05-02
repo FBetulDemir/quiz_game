d0d13c7a0748275fefcf3464148a20f2d9e4c411
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2024-05-02 10:26:36 +0200

    I start with timer component

---
+++ quiz_game/src/components/Timer.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export const Timer = ({ onRestart }) => {
+  const initialTime = 5 * 60;
+  const [timeRemaining, setTimeRemaining] = useState(initialTime);
+  let timerId;
+
+  return (
+    <div>
+      <p>
+       
+      </p>
+    </div>
+  );
+};
