3e41b2feb733d2865e4b1b3d581a46ccfa324863
FBetulDemir <fatmabetuld@gmail.com>
2024-05-09 11:39:11 +0200

    Deleted my last changes

--- quiz_game/src/components/Quiz.js
+++ quiz_game/src/components/Quiz.js
@@ -4,14 +4,12 @@ import Score from "./Score";
 
 const Quiz = ({currentQuestionIndex, setCurrentQuestionIndex, setScore, score, quizFinished, setQuizFinished }) => {
 
-    const [isCorrect, setIsCorrect] = useState(null);
+    
 
     const handleAnswerButtonClick = (selectedAnswer) => {  
         if (selectedAnswer === QuestionList[currentQuestionIndex].answer) {
           setScore (score + 1);
-          setIsCorrect(true);
-        }else{
-          setIsCorrect(false);
+          
         };
     
         const nextQuestionIndex = currentQuestionIndex + 1;
@@ -42,9 +40,6 @@ const Quiz = ({currentQuestionIndex, setCurrentQuestionIndex, setScore, score, q
             <div className="options-container">
                 {QuestionList[currentQuestionIndex].options.map((option) =>(
                     <button 
-                        style={{
-                            backgroundColor: option === QuestionList[currentQuestionIndex].answer ? "green": "red"
-                        }}
                         className="answer-btn"
                         key={option}
                         onClick={()=> handleAnswerButtonClick(option)}>
