e97948b0b1dc4be652a0b5fc84a37be6d38e2e7f
FBetulDemir <fatmabetuld@gmail.com>
2024-05-10 09:35:47 +0200

    Created the next button

--- quiz_game/src/components/NavigationButtons.js
+++ quiz_game/src/components/NavigationButtons.js
@@ -1,5 +1,18 @@
-const  = () => {
-    return (  );
+import {useState} from "react";
+import QuestionList from "./QuestionList";
+
+const  NavigationButton = ({currentQuestionIndex, setCurrentQuestionIndex,}) => {
+    const handleNextButtonClick = () => {
+        const nextQuestionIndex = currentQuestionIndex + 1;
+        if (nextQuestionIndex < QuestionList.length){
+            setCurrentQuestionIndex(nextQuestionIndex);
+        }
+    };
+    return ( 
+        <div className="navigation-buttons">
+            <button onClick={handleNextButtonClick} disabled={QuestionList.id > 10}>Next</button>
+        </div>
+     );
 }
  
-export default ;
+export default NavigationButton;
