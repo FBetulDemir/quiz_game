69f3528756b67244eef47c24ef57003fb48fa257
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2024-05-11 18:14:24 +0200

    fixed start.js as Celestine did before folder problem

--- quiz_game/src/components/Start.js
+++ quiz_game/src/components/Start.js
@@ -1,17 +1,23 @@
-import { useRef } from "react";
+import { useRef,useState } from "react";
 
 
 
-export default function Start({ setUsername }) {
+export default function Start({ onNameChange }) {
+  const [name, setName] = useState(''); 
+  const handleInputChange = (event) => { 
+    const newName = event.target.value; 
+    setName(newName); 
+    onNameChange(newName); };
   const inputRef = useRef();
 
   const handleClick = () => {
-    inputRef.current.value && setUsername(inputRef.current.value);
+   // inputRef.current.value && setUsername(inputRef.current.value);
   };
 
   return (
-    <div>
-      <input className="button-start" type="text" value={name} onChange={handleInputChange} />
+    <div className="start">
+      <input type="text" value={name} onChange={handleInputChange} />
+      
     </div>
   );
 }
